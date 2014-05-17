04b7e2538315a20b3b334c10cee3ed0b5852d988
kimihito <tatsurotamashiro@gmail.com>
2014-05-17 17:54:35 +0900

    push['status']だった

--- app.rb
+++ app.rb
@@ -6,9 +6,9 @@ post '/' do
   push = JSON.parse(params[:payload])
   case push['status']
   when 'pass'
-    status_message = "<span class='label label-sucess>#{status.upcase}</span>"
+    status_message = "<span class='label label-sucess>#{push['status'].upcase}</span>"
   when 'fail'
-    status_message = "<span class='label label-important>#{status.upcase}</span>"
+    status_message = "<span class='label label-important>#{push['status'].upcase}</span>"
   end
   message = "#{status_message} #{push['message']} by @#{push['committer']} <a href='#{push['commit_url']}' target='_black'>Commit</a> <a href='#{push['build_url']}' target='_blank'>Build</a>"
   Idobata.hook_url = ENV['IDOBATA_HOOK_URL']
